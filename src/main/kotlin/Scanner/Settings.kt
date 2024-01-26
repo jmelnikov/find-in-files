3e99e7e5e28d8a88247733aba893a3444eb04861
Yuriy Melnikov <melnikov.j@gmail.com>
2024-01-26 20:18:40 +0300

    Файлы, в которых точно не нужно искать ссылки

--- src/main/kotlin/Scanner/Settings.kt
+++ src/main/kotlin/Scanner/Settings.kt
@@ -4,4 +4,36 @@ object Settings {
     const val FILES_PATH = "D:\\Projects\\kotlin\\find-urls-in-files"
     const val RESULT_PATH = "D:\\Projects\\kotlin\\find-urls-in-files\\docker\\result"
 
+    val EXCLUDED_PATHS = listOf(
+        ".idea",
+        ".git",
+        "vendor"
+    )
+
+    val EXCLUDED_FILES = listOf(
+        ".env",
+    )
+
+    val EXCLUDED_EXTENSION = listOf(
+        "mp3",
+        "mp4",
+        "avi",
+        "mkv",
+        "mov",
+        "png",
+        "jpg",
+        "jpeg",
+        "gif",
+        "svg",
+        "ico",
+        "gitignore",
+        "lock",
+        "xml",
+        "css",
+        "scss"
+    )
+
+    val EXCLUDED_PATH_REGEX = listOf(
+        Regex(".*min\\.js$"),
+    )
 }
